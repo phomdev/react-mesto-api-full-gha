e81b460e09329e136a9fbf403e8d845a63222151
Artem Rudenko <phomdev@yandex.ru>
2023-04-01 22:08:45 +0300

    Изменён с HashRouter на BrowserRouter

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import './index.css'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter>
+  <BrowserRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </HashRouter>
+  </BrowserRouter>
 );
